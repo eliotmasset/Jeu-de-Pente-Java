7d951044d7e39af41979e8856b118fa5bdf3e81b
Hélène GRANGE <MTC@MacBook-Air-de-Helene.local>
2020-10-25 17:57:42 +0100

    Affiche le scoreboard avec une scrollBar si trop de pseudos enregistrer

--- src/zoneDessinScoreBoard.java
+++ src/zoneDessinScoreBoard.java
@@ -8,7 +8,9 @@ import java.util.regex.*;
 class zoneDessinScoreBoard extends JPanel
 {
     private int size;
-    private JLabel titre, Scores;
+    private JLabel titre;
+    private JTextArea Scores;
+    private JScrollPane scrollPane;
     
 	zoneDessinScoreBoard(int _size)
 	{
@@ -16,23 +18,31 @@ class zoneDessinScoreBoard extends JPanel
 		size=_size;
         setSize(size,size);
         titre = new JLabel("ScoreBoard : ");
-        Scores = new JLabel("");
-	}
-	
-	@Override
-	public void paintComponent(Graphics g)
-	{
-        super.paintComponent(g);
-        ScorePlayers();
+        Scores = new JTextArea("");
+        scrollPane = new JScrollPane(Scores);
         affiche_ScoreBoard();
-    }
+        ScorePlayers();
+	}
     
     private void affiche_ScoreBoard()
 	{
         Font font1 = new Font("Arial",Font.BOLD,40);
-        Font font2 = new Font("Arial",Font.BOLD,18);
+        Font font2 = new Font("Courier",Font.BOLD,18);
 
         setBackground(new Color(34,0,111));
+
+        Scores.setForeground(new Color(255,255,255));
+        Scores.setEditable(false);
+        Scores.setOpaque(false);
+        Scores.setFont(font2);
+        Scores.setBounds(size/4, size/6,(6*size)/10,(8*size)/10);
+
+        scrollPane.setBounds(size/4, size/6,(6*size)/10,(8*size)/10);
+        scrollPane.setBackground(new Color(34,0,111));
+        scrollPane.getViewport().setBackground(new Color(34,0,111));
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        scrollPane.setViewportBorder(BorderFactory.createEmptyBorder());
+        
 
         titre.setText("ScoreBoard : ");
         titre.setForeground(new Color(255,255,255));
@@ -40,14 +50,8 @@ class zoneDessinScoreBoard extends JPanel
         titre.setBounds(size/2-titre.getText().length()*11, size/20,titre.getText().length()*30,titre.getText().length()*4);
         titre.setFont(font1);
 
-        
-        Scores.setForeground(new Color(255,255,255));
-        Scores.setOpaque(false);
-        Scores.setBounds(size/4, size/10,titre.getText().length()*60,titre.getText().length()*40);
-        Scores.setFont(font2);
-
         add(titre);
-        add(Scores);
+        add(scrollPane);
     }
     
     public void ScorePlayers()
@@ -75,16 +79,17 @@ class zoneDessinScoreBoard extends JPanel
                 String[] items = p.split(reader, 10);
                 if(items.length>=3)
                 {
-                    String addBuilder="";
+                    String addBuilder="   ";
                     addBuilder+=items[0]+"               ";
                     for(int i=0;i<9-items[0].length();i++)
                         addBuilder+=" ";
-                    addBuilder+=items[2]+"<br><br>";
+                    addBuilder+=items[2]+"\n\n";
                     builder.append(addBuilder);
                 }
                 reader = br.readLine();
             }
-            Scores.setText("<html><pre>Pseudos :        Nombre de points :<br><br><br>"+builder.toString()+"</pre></html>");
+            Scores.setText("Pseudos :           Nombre de points :\n\n\n"+builder.toString());
+            Scores.setCaretPosition(0);
             br.close();
             
         }
